c1a35d46f049d9a72761d3e0d8f97af0eaabd4fe
ishaandwivedi1234 <ishaan.dwivedi99@gmail.com>
2020-10-12 19:39:14 +0530

    bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const home = require('./routes/home')
 const auth =require('./routes/auth')
-require('./db/db')()
+// require('./db/db')()
 
 app.use(express.json())
 app.use('/', home)
