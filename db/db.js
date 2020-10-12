30ed2667a086365c76947c0fb6efbf7b43ae6e3b
ishaandwivedi1234 <ishaan.dwivedi99@gmail.com>
2020-10-12 20:42:31 +0530

    db

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 module.exports = function () {
-    mongoose.connect('mongodb://localhost/dev_stories', { useUnifiedTopology: true,useNewUrlParser: true })
+    const url = process.env.MONGODB_URL  || 'mongodb://localhost/dev_stories' ;
+    mongoose.connect(url, { useUnifiedTopology: true,useNewUrlParser: true })
         .then(() => console.log('Conneted to mongoDb'))
         .catch(err => console.log('cant connect to mongodb'))
 }
