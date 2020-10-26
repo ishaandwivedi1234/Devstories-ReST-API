a2e432c40c2ccf59ccd687bcbda7422ab89fa815
ishaandwivedi1234 <ishaan.dwivedi99@gmail.com>
2020-10-26 22:09:55 +0530

    likes

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express')
 const route = express.Router();
 const auth = require('../autherisation/authztion')
 const {Post,validatePost} = require('../models/post')
+const _ = require('lodash');
 route.post('/', auth, async (req, res) => {
     const { error } = validatePost(req.body)
     if (error) return res.status(400).send(error.details[0].message);
@@ -63,13 +64,15 @@ route.get('/:id', auth, async(req, res) => {
     }
 })
 
-route.put('/:id/:userId', auth, async (req, res) => {
+route.put('/unlike/:id/:userId', auth, async (req, res) => {
     const { error } = validatePost(req.body)
     if(error) res.status(400).send(error.details[0].message)
     try {
         let post = await Post.findById(req.params.id)
         if (!post) return res.status(401).send('post not found')
+
         likes = post.likedBy;
+        _.remove(likes, (e) => e === req.params.userId.toString());
         post = await post.set({
         userId: post.userId,
         userProfilePic: post.userProfilePic,
@@ -90,7 +93,34 @@ route.put('/:id/:userId', auth, async (req, res) => {
         res.status(500).send(ex.message)
     }
 })
+route.put('/like/:id/:userId', auth, async (req, res) => {
+    const { error } = validatePost(req.body)
+    if(error) res.status(400).send(error.details[0].message)
+    try {
+        let post = await Post.findById(req.params.id)
+        if (!post) return res.status(401).send('post not found')
 
+        likes.push(req.params.userId.toString());
+        post = await post.set({
+        userId: post.userId,
+        userProfilePic: post.userProfilePic,
+        userName: post.userName,
+        bgImage: post.bgImage,
+        fontColor: post.fontColor,
+        // optional parameters 
+        postDescription: post.postDescription,
+        likedBy: likes,
+        images: post.images,
+        files: post.files,
+        gifs: post.gifs
+        })
+        await post.save();
+        res.send(post)
+    } catch (ex) {
+        console.log('something failed..')
+        res.status(500).send(ex.message)
+    }
+})
 
 route.delete('/:id', auth, async(req, res) => {
 
